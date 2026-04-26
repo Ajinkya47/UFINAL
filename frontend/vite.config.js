386ebf251aa9d82a2ae61da242255e01951f492c
Ajinkya  Kokate <148894276+Ajinkya47@users.noreply.github.com>
2026-04-26 07:23:21 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/static/' : '/',
+export default defineConfig({
+  base: './',   // ⭐ required for Amplify
   plugins: [react()],
   server: {
     port: 3000,
@@ -19,4 +18,4 @@ export default defineConfig(({ command }) => ({
       }
     }
   }
-}))
+})
